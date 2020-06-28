b5680bf4a8f6ea51a4b762f57091f5b7f2e3e18f
shamita1512 <chemica1cooki3@gmail.com>
2020-06-28 14:34:06 +0530

    made both tabs synchronized

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 var ball;
-
+var position,database;
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
+    var posBall = database.ref("ball/position");
+    posBall.on("value",readOp,showError);
+    //listener function
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
 }
@@ -9,21 +14,34 @@ function setup(){
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        x: ball.x + x, 
+        y: ball.y + y
+    })
+}
+
+function readOp(data){
+    position = data.val();
+    //predefined function that takes value from setup
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError(){
+    console.log("error");
 }
